33b7491cc98d7e2ff27f47a320d8f401e31b99a6
Shen Xiaochun <shendeveloper@gmail.com>
2017-03-23 14:11:28 +0800

    add tab view controller

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -59,6 +59,8 @@ class ConversionViewController: UIViewController, UITextFieldDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        print("ConversionViewController loaded its view.")
+        
         updateCelsiusLabel()
     }
     
---
+++ WorldTrotter/MapViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Shen Xiaochun on 2017/3/23.
+//  Copyright © 2017年 Sigutian. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("MapViewController loaded its view.")
+    }
+    
+}
